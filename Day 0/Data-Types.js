b5c9e4c2935e7c9c8768a812d17ad46af65241d4
Pradyuman Singh <66694735+pradyumansingh989@users.noreply.github.com>
2021-02-16 21:08:01 +0530

    added day 0 code solution

---
+++ Day 0/Data-Types.js
@@ -0,0 +1,17 @@
+let secondInteger = 12
+let secondDecimal = 4.32
+let secondString = "is the best place to learn and practice coding!"
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    const answer1 = firstInteger + Number(secondInteger);
+    console.log(answer1);
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    const answer2 = firstDecimal + Number(secondDecimal);
+    console.log(answer2);
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    const answer3 = firstString + secondString;
+    console.log(answer3);
+}
