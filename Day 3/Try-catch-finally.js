9702096f6c27cad53ba2754d5a60d308955c5475
Pradyuman Singh <66694735+pradyumansingh989@users.noreply.github.com>
2021-02-17 10:48:02 +0530

    added day 3 solutions

---
+++ Day 3/Try-catch-finally.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    if (typeof s === "string") {
+    console.log(
+      s
+        .split("")
+        .reverse()
+        .join("")
+    );
+  } else {
+    console.log("s.split is not a function" + "\n" + s);
+  }
+}
